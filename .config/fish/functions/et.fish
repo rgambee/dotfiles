2a316397a1744ada7b7b9574490373d6aca9481e
Robert Gambee <robert@futuresearch.ai>
2025-11-23 14:44:37 -0500

    Save alias for listing files as a tree

---
+++ .config/fish/functions/et.fish
@@ -0,0 +1,3 @@
+function et --wraps='eza -T --hyperlink -l --time-style=long-iso' --description 'alias et=eza -T --hyperlink -l --time-style=long-iso'
+    eza -T --hyperlink -l --time-style=long-iso $argv
+end
