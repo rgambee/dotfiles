f960180c8e59e028249405df7499691fb84074fe
Robert Gambee <robert@futuresearch.ai>
2026-06-01 09:03:30 -0400

    Create offline alias for uvx

---
+++ .config/fish/functions/uvx.fish
@@ -0,0 +1,3 @@
+function uvx --wraps='uv run --offline --project /Users/robert/tools/uv ' --description 'alias uvx=uv run --offline --project /Users/robert/tools/uv '
+    uv run --offline --project /Users/robert/tools/uv  $argv
+end
