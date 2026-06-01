985fda9a653949128d8c505cfdad5fcea0e357c3
Robert Gambee <robert@futuresearch.ai>
2026-06-01 10:16:20 -0400

    Create offline alias for pnpx

---
+++ .config/fish/functions/pnpx.fish
@@ -0,0 +1,3 @@
+function pnpx --wraps='pnpm --dir /Users/robert/tools/pnpm --offline ' --description 'alias pnpx=pnpm --dir /Users/robert/tools/pnpm --offline '
+    pnpm --dir /Users/robert/tools/pnpm --offline  $argv
+end
